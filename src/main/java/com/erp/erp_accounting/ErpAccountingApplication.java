e03d9a798e767506680d7027fd2549c8cb73b1b6
zy23n <monapear@naver.com>
2025-12-21 19:12:05 +0900

    Initial project setup

---
+++ src/main/java/com/erp/erp_accounting/ErpAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.erp.erp_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/erp_accounting/ErpAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.erp_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
