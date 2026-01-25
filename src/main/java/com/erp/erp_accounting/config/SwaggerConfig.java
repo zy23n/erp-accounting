bfa104bc75abdc8c814fa0c435fd491bf2ba4dd5
zy23n <monapear@naver.com>
2026-01-26 02:04:59 +0900

    docs: add Swagger OpenAPI config

---
+++ src/main/java/com/erp/erp_accounting/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.erp.erp_accounting.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ERP 회계/인사 API")
+                        .description("ERP 시스템 회계 및 인사 관리 API 문서")
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+    }
+}
